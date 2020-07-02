9cc0cb15cd867cc1a4389ce3d7cbfd730df384ca
mathuringagnon <gagnonmathurin@gmail.com>
2020-07-02 12:59:50 -0400

    commit from R

--- ui.R
+++ ui.R
@@ -24,16 +24,25 @@ navbarPage("Exploration of US Land",
                        )
                      ),
                      tags$h3(
-                        tags$p("In recent centuries technology has vastly improved, aiding in the explosion of the human population which,
+                        tags$p("   In recent centuries technology has vastly improved, aiding in the explosion of the human population which,
                                now in 2020, has surpassed 7.6 billion globally. This explosion of population has undoubtedly affected many ecosystems,
                                and as humans continue to expand agricultural and urban land use, examining which ecosystems have been impacted is becoming 
-                               increasingly urgent. ", "text-indent: 1.5em"
+                               increasingly urgent. "
                               ),
                         tags$br(),
                         tags$p("   The United States is the fourth largest country in the world and has 2.3 billion acres of land, with the lower 48 states comprising
                                 1.9 billion of those acres. In our web app we explore how the ~1,000 ecosystems of the US have changed, where the changes have been 
                                 the greatest, as well as how the land of the US has is used between urban settings, agriculture, and natural.")
                      )
+              ),
+              column(4, align = "right",
+                     tags$img(src = 'aerial-architectural-design-architecture-buildings-373912.jpg', height = "100%", width = "80%")
+              ),
+              column(4, align = "middle",
+                     tags$img(src = 'photo-of-green-field-near-mountains-974314.jpg', height = "100%", width = '80%')
+              ),
+              column(4, align = "left",
+                     tags$img(src = 'nature-forest-trees-park-38136.jpg', height = '100%', width = '80%')
               )
             )
    ),
@@ -68,13 +77,16 @@ navbarPage("Exploration of US Land",
                        ),
         absolutePanel( id = "controls", class = "panel panel-default", fixed = TRUE,
                       draggable = TRUE, top = "auto", left = 10, right = "auto", bottom = 10,
-                      width = 160, height = 140,
+                      width = 160, height = 170, offset = 0.5,
                       
-                      h2("  Color By: "),
+                      tags$h3(
+                        tags$b("Color By: ")
+                      ),
                       radioButtons("colorBy", label = NULL,
                                     choices = list("    Percent Natural" = 1, "    Percent Agriculture" = 2, "    Percent Urban" = 3),
                                     selected = 1
-                                    )
+                      ),
+                      tags$h6('(Wait for Map to Load)')
         
             #sliderInput("percentSelect", "Percent",
             #            0, 100, c(0, 100), step = 0.01),
@@ -83,5 +95,39 @@ navbarPage("Exploration of US Land",
     ),
     tabPanel("About",
              #this page has about what code we used how we created this page
+             fluidRow(
+               column(7, align = 'left', offset = 1,
+                      tags$h1(
+                        tags$b('About Our App')
+                      ),
+                      tags$h3(
+                        tags$ul(
+                          tags$li('App developed by Mathurin Gagnon (coding and design) and Randy Swaty (data preparation)'),
+                          tags$br(),
+                          tags$li('We developed the app in ', tags$a(href = 'https://rstudio.com/', "R-Studio"),  ' using the Shiny package for interactivity.'),
+                          tags$br(),
+                          tags$li('Project developed for internship with the ', tags$a(href = 'https://google.com', 'Conservation Data Lab'), '.'),
+                          tags$br()
+                        )
+                      ),
+                      tags$h1(
+                        tags$b('Input Data:')
+                      ),
+                      tags$h3(
+                        tags$ul(
+                          tags$li('LANDFIRE Biophysical Settings for historical ecosystems and Existing Vegetation Types for conversion ', tags$br(), '(learn more at ',
+                            tags$a(href = 'https://www.landfire.gov/', 'www.landfire.gov'), ').' 
+                          ),
+                          tags$br(),
+                          tags$li('US County Shape File ', tags$a( href = 'https://community.esri.com/thread/24614/', 'download '), 'page.'),
+                          tags$br(),
+                          tags$li('US County Data can be found ', tags$a(href = 'https://www.google.com/', 'here'), ".")
+                        )
+                      )
+               ),
+               column(3, align = 'right', offset = 0,
+                 tags$img(src = 'me.PNG', height = '100%', width = "80%")
+               )
+             )
     )
 )
